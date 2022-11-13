7b04669b591bb5bac096dc06a71421353adc53cf
Shunsuke <shunsuke.takasu@gmail.com>
2022-11-13 23:30:06 +0900

    Add:Next.js Blog

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,11 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <h3>Hello World Next.js</h3> 
+        <p>
+          最近学び初めたShunと申します｡<br/>
+          初心者なので分からないことが多いですが頑張ります!
+        </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
